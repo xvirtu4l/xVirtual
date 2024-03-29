08da22d89a43f6c530f5d8c0b2a89cee792a5910
xvirtu4l <162694865+xvirtu4l@users.noreply.github.com>
2024-03-29 09:21:28 +0700

    Add files via upload

---
+++ commons/env.php
@@ -0,0 +1,26 @@
+<?php 
+
+// Khai báo các biến môi trường dùng Global
+define('PATH_CONTROLLER',   __DIR__ . '/../controllers/');
+define('PATH_MODEL',        __DIR__ . '/../models/');
+define('PATH_VIEW',         __DIR__ . '/../views/');
+
+define('PATH_CONTROLLER_ADMIN',   __DIR__ . '/../admin/controllers/');
+define('PATH_MODEL_ADMIN',        __DIR__ . '/../admin/models/');
+define('PATH_VIEW_ADMIN',         __DIR__ . '/../admin/views/');
+
+define('PATH_UPLOAD',         __DIR__ . '/../');
+
+define('BASE_URL',          '/xVirtual/');
+define('BASE_URL_ADMIN',    '/xVirtual/admin/');
+
+define('DB_HOST',       'localhost');
+define('DB_PORT',       '3306');
+define('DB_USERNAME',   'root');
+define('DB_PASSWORD',   '');
+define('DB_NAME',       'duan1');
+
+define('STATUS_DRAFT',      'draft');
+define('STATUS_PUBLISHED',  'published');
+
+
