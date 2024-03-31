53a18c329c09467f1cae164f560dc752e57be5a7
hienbhph46051 <hienbhph46051@fpt.edu.vn>
2024-04-01 05:38:20 +0700

    edit shop

--- models/Variant.php
+++ models/Variant.php
@@ -1,59 +1,81 @@
-<?php 
-    function selectAllVariants($id = null) {
-        try {
-            $sql = "SELECT * FROM variant";
-            if ($id !== null) {
-                $sql .= " WHERE id_pro = :id";
-            }
-            $stmt = $GLOBALS['conn']->prepare($sql);
-            if ($id !== null) {
-                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-            }
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            die($e->getMessage());
+<?php
+function selectAllVariants($id = null) {
+    try {
+        $sql = "SELECT * FROM variant";
+        if ($id !== null) {
+            $sql .= " WHERE id_pro = :id";
         }
+        $stmt = $GLOBALS['conn']->prepare($sql);
+        if ($id !== null) {
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        }
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+function countVariant($id)
+{
+    try {
+        $sql = "SELECT COUNT(*) FROM variant WHERE id_pro = :id_pro AND quantity = '0'";
+        $stmt = $GLOBALS['conn']->prepare($sql);
+        $stmt->bindParam(':id_pro', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $number_of_rows = $stmt->fetchColumn();
+        return $number_of_rows;
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+function selectDistinctStorage($id) {
+    try {
+        $sql = "SELECT DISTINCT storage FROM variant WHERE id_pro = :productId";
+        $stmt = $GLOBALS['conn']->prepare($sql);
+        $stmt->bindParam(':productId', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+function selectDistinctColors($id) {
+    try {
+        $sql = "SELECT DISTINCT color FROM variant WHERE id_pro = :productId";
+        $stmt = $GLOBALS['conn']->prepare($sql);
+        $stmt->bindParam(':productId', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+function OutofStock($id) {
+    try {
+        $sql = "SELECT DISTINCT color FROM variant WHERE id_pro = :productId AND quantity <= 0";
+        $stmt = $GLOBALS['conn']->prepare($sql);
+        $stmt->bindParam(':productId', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+function out_of_quality($id)
+{
+    try {
+
+
+    } catch (PDOException $e) {
+        die($e->getMessage());
     }
 
-    function selectDistinctStorage($id) {
-        try {
-            $sql = "SELECT DISTINCT storage FROM variant WHERE id_pro = :productId";
-            $stmt = $GLOBALS['conn']->prepare($sql);
-            $stmt->bindParam(':productId', $id, PDO::PARAM_INT);
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    function selectDistinctColors($id) {
-        try {
-            $sql = "SELECT DISTINCT color FROM variant WHERE id_pro = :productId";
-            $stmt = $GLOBALS['conn']->prepare($sql);
-            $stmt->bindParam(':productId', $id, PDO::PARAM_INT);
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-    function OutofStock($id) {
-        try {
-            $sql = "SELECT DISTINCT color FROM variant WHERE id_pro = :productId AND quantity <= 0";
-            $stmt = $GLOBALS['conn']->prepare($sql);
-            $stmt->bindParam(':productId', $id, PDO::PARAM_INT);
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
+}
 
 ?>
